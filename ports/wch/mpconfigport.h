96a8ed875be7cbca4ed0eaa637f12e46fef1cd57
Alessandro Gatti <a.gatti@frob.it>
2024-10-09 01:10:36 +0200

    wch/Makefile: Use platform-specific GC Helper.
    This commit lets the port use the RV32I-specific GC helper
    implementation, instead of relying on the setjmp-based default.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/wch/mpconfigport.h
+++ ports/wch/mpconfigport.h
@@ -14,7 +14,6 @@
 #define MICROPY_HELPER_REPL               (1)
 #define MICROPY_MODULE_FROZEN_MPY         (0)
 #define MICROPY_ENABLE_EXTERNAL_IMPORT    (1)
-#define MICROPY_GCREGS_SETJMP             (1)
 
 #define MICROPY_ALLOC_PATH_MAX            (256)
 #define MICROPY_ALLOC_PARSE_CHUNK_INIT    (16)

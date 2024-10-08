be31824bb43cbea57bb4a6965d03fe0fe048294a
Alessandro Gatti <a.gatti@frob.it>
2024-10-09 01:00:42 +0200

    wch/main: Fix usage of the friendy REPL and soft reboot on exit.
    This commit fixes the usage of the REPL, letting the firmware image
    link, and on interpreter exit perform a soft reboot (following the other
    ports' behaviour in this case).
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/wch/main.c
+++ ports/wch/main.c
@@ -3,6 +3,7 @@
 #include "py/gc.h"
 #include "py/mperrno.h"
 #include "py/stackctrl.h"
+#include "shared/readline/readline.h"
 #include "shared/runtime/gchelper.h"
 #include "shared/runtime/pyexec.h"
 
@@ -12,23 +13,39 @@ static char heap[4096];
 int main(int argc, char **argv) {
     // Initialise the MicroPython runtime.
     mp_stack_ctrl_init();
+
+soft_reset:
     gc_init(heap, heap + sizeof(heap));
     mp_init();
+    readline_init0();
 
-    // Start a normal REPL; will exit when ctrl-D is entered on a blank line.
-    for (;;) {pyexec_friendly_repl();
+    #if MICROPY_ENABLE_COMPILER
+    // Main script is finished, so now go into REPL mode.
+    // The REPL mode can change, or it can request a soft reset.
+    for (;;) {
+        if (pyexec_mode_kind == PYEXEC_MODE_RAW_REPL) {
+            if (pyexec_raw_repl() != 0) {
+                break;
+            }
+        } else {
+            if (pyexec_friendly_repl() != 0) {
+                break;
+            }
+        }
     }
+    #endif
 
     // Deinitialise the runtime.
     gc_sweep_all();
     mp_deinit();
+
+    goto soft_reset;
     return 0;
 }
 
 // Handle uncaught exceptions (should never be reached in a correct C implementation).
 void nlr_jump_fail(void *val) {
-    for (;;)
-    {
+    for (;;) {
     }
 }
 

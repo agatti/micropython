b2871b0c20917e52713e0a2b4efd538686407ab6
Alessandro Gatti <a.gatti@frob.it>
2024-10-09 02:57:43 +0200

    wch/bsp: Update the BSP with the latest version from MRS Studio.
    This commit updates the BSP to the version that is shipped with
    MounRiver Community Studio 1.80, moving the BSP forward by about three
    years.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/wch/bsp/src/ch32v30x_it.c
+++ ports/wch/bsp/src/ch32v30x_it.c
@@ -2,10 +2,12 @@
 * File Name          : ch32v30x_it.c
 * Author             : WCH
 * Version            : V1.0.0
-* Date               : 2021/06/06
+* Date               : 2024/03/06
 * Description        : Main Interrupt Service Routines.
+*********************************************************************************
 * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* SPDX-License-Identifier: Apache-2.0
+* Attention: This software (modified or not) and binary are used for
+* microcontroller manufactured by Nanjing Qinheng Microelectronics.
 *******************************************************************************/
 #include "ch32v30x_it.h"
 
@@ -20,6 +22,8 @@ void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
  * @return  none
  */
 void NMI_Handler(void) {
+    while (1) {
+    }
 }
 
 /*********************************************************************

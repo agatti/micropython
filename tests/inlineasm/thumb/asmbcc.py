324557c370f0addb0ec74f8cabee783463293101
Alessandro Gatti <a.gatti@frob.it>
2026-03-25 21:47:37 +0100

    py/emitinlinethumb: Shrink condition code lookups.
    This commit optimises the lookup of opcodes' condition codes, in order
    to take up less code than before.
    
    The original data was held in a table containing the condition code
    value (an incrementing 0-based integer) and the two condition ASCII
    characters.  Given that the condition code value also matches the
    entry's index in the table, that can be safely omitted.
    
    This saves 52 bytes when compiled for Thumb.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- tests/inlineasm/thumb/asmbcc.py
+++ tests/inlineasm/thumb/asmbcc.py
@@ -1,9 +1,11 @@
-# test bcc instructions
-# at the moment only tests beq, narrow and wide versions
+# test bcc instructions, narrow and wide versions
+
+
+RESULT = []
 
 
 @micropython.asm_thumb
-def f(r0):
+def test_beq(r0):
     mov(r1, r0)
 
     mov(r0, 10)
@@ -23,7 +25,52 @@ def f(r0):
     label(end)
 
 
-print(f(0))
-print(f(1))
-print(f(2))
-print(f(3))
+RESULT.append("BEQ")
+for i in range(4):
+    RESULT.append(test_beq(i))
+
+
+TEMPLATE = """
+@micropython.asm_thumb
+def t(r0):
+    mov(r1, r0)
+
+    mov(r0, 10)
+    cmp(r1, 1)
+    b{}(next1)
+
+    b(end)
+
+    label(next1)
+
+    mov(r0, 20)
+    cmp(r1, 2)
+    b{}_n(next2)
+
+    b(end)
+
+    label(next2)
+    mov(r0, 30)
+    cmp(r1, 3)
+    b{}_w(next3)
+
+    b(end)
+
+    label(next3)
+    mov(r0, 0)
+
+    label(end)
+"""
+
+try:
+    for code in ("ne", "cs", "cc", "mi", "pl", "vs", "vc", "hi", "ls", "ge", "lt", "gt", "le"):
+        exec(TEMPLATE.format(code, code, code))
+        RESULT.append("B" + code.upper())
+        for i in range(4):
+            RESULT.append(t(i))
+except MemoryError:
+    print("SKIP-TOO-LARGE")
+    raise SystemExti
+
+for line in RESULT:
+    print(line)

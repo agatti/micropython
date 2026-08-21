a752b5675eb8bfedb5133c15dd79f66755e6b19d
Alessandro Gatti <a.gatti@frob.it>
2026-08-21 03:51:29 +0200

    esp32: Initial WiFi DPP implementation.
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/esp32/network_wlan.c
+++ ports/esp32/network_wlan.c
@@ -45,6 +45,9 @@
 #if MICROPY_PY_NETWORK_WLAN_CSI
 #include "network_wlan_csi.h"
 #endif
+#if MICROPY_PY_NETWORK_WLAN_DPP
+#include "esp_dpp.h"
+#endif
 #include "esp_log.h"
 #include "esp_psram.h"
 #if !CONFIG_ESP_HOSTED_ENABLED
@@ -96,8 +99,92 @@ static uint8_t wifi_sta_reconnects;
 #define WIFI_PROTOCOL_DEFAULT (WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N)
 #endif
 
+#if MICROPY_PY_NETWORK_WLAN_DPP
+
+enum {
+    DPP_STATE_WAIT,
+    DPP_STATE_READY,
+    DPP_STATE_ERROR,
+};
+
+static struct wifi_dpp_state_t {
+    // This must be allocated and freed via heap_caps_malloc/heap_caps_free.
+    char *qr_code_buffer;
+    ssize_t qr_code_length; // -1 indicates failure
+    wifi_config_t configuration;
+    volatile bool listening;
+    volatile int state;
+    volatile int error;
+} wifi_dpp_state;
+
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 5, 0))
+#define EVT_DPP_URI_READY (WIFI_EVENT_DPP_URI_READY)
+#define EVT_DPP_CFG_RECVD (WIFI_EVENT_DPP_CFG_RECVD)
+#define EVT_DPP_FAILED (WIFI_EVENT_DPP_FAILED)
+#else
+#define EVT_DPP_URI_READY (ESP_SUPP_DPP_URI_READY)
+#define EVT_DPP_CFG_RECVD (ESP_SUPP_DPP_CFG_RECVD)
+#define EVT_DPP_FAILED (ESP_SUPP_DPP_FAIL)
+#endif
+
+// Merge this with the main WiFi event handler once ESP-IDF 5.5.x is the minimum
+// supported version.
+static void esp_wifi_dpp_callback(esp_supp_dpp_event_t evt, void *data) {
+    // URI_READY event is sent during bootstrap and within the scope of
+    // WLAN.dpp_start, so the callback shouldn't be able to receive stray
+    // URI_READY events.
+    if ((int)evt != EVT_DPP_URI_READY && !wifi_dpp_state.listening) {
+        return;
+    }
+
+    switch ((int)evt) {
+        case EVT_DPP_URI_READY: {
+            assert(!wifi_dpp_state.qr_code_buffer && wifi_dpp_state.qr_code_length == 0 && "QR code variables went out of sync");
+            const wifi_event_dpp_uri_ready_t *event_data = (const wifi_event_dpp_uri_ready_t *)data;
+            char *new_buffer = heap_caps_malloc(event_data->uri_data_len, MALLOC_CAP_DEFAULT);
+            if (!new_buffer) {
+                wifi_dpp_state.qr_code_length = -1;
+                return;
+            }
+            wifi_dpp_state.qr_code_buffer = new_buffer;
+            memcpy(wifi_dpp_state.qr_code_buffer, event_data->uri, event_data->uri_data_len);
+            wifi_dpp_state.qr_code_length = (ssize_t)event_data->uri_data_len;
+            assert(wifi_dpp_state.state == DPP_STATE_WAIT && "DPP state out of sync");
+            break;
+        }
+        case EVT_DPP_CFG_RECVD: {
+            // Provisioning data obtained.
+            memcpy(&wifi_dpp_state.configuration, &((const wifi_event_dpp_config_received_t *)data)->wifi_cfg, sizeof(wifi_config_t));
+            esp_err_t result = esp_supp_dpp_stop_listen();
+            if (result != ESP_OK) {
+                wifi_dpp_state.state = DPP_STATE_ERROR;
+                wifi_dpp_state.error = result;
+            } else {
+                wifi_dpp_state.listening = false;
+                result = esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_dpp_state.configuration);
+                if (result != ESP_OK) {
+                    wifi_dpp_state.state = DPP_STATE_ERROR;
+                    wifi_dpp_state.error = result;
+                }
+            }
+            wifi_dpp_state.state = DPP_STATE_READY;
+            break;
+        }
+        case EVT_DPP_FAILED:
+            const wifi_event_dpp_failed_t *event_data = (const wifi_event_dpp_failed_t *)data;
+            wifi_dpp_state.state = DPP_STATE_ERROR;
+            wifi_dpp_state.error = event_data->failure_reason;
+            break;
+
+        default:
+            return;
+    }
+}
+#endif
+
 // This function is called by the system-event task and so runs in a different
-// thread to the main MicroPython task.  It must not raise any Python exceptions.
+// thread to the main MicroPython task.  It must not raise any Python exceptions
+// or allocate any memory in MicroPython's own heap.
 static void network_wlan_wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     switch (event_id) {
         case WIFI_EVENT_STA_START:
@@ -183,6 +270,14 @@ static void network_wlan_wifi_event_handler(void *event_handler_arg, esp_event_b
         case WIFI_EVENT_AP_STOP:
             wlan_ap_obj.active = false;
             break;
+
+        #if MICROPY_PY_NETWORK_WLAN_DPP && (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 5, 0))
+        case WIFI_EVENT_DPP_URI_READY:
+        case WIFI_EVENT_DPP_CFG_RECVD:
+        case WIFI_EVENT_DPP_FAILED:
+            esp_wifi_dpp_callback(event_id, event_data);
+            break;
+        #endif
 
         default:
             break;
@@ -309,11 +404,188 @@ static mp_obj_t network_wlan_active(size_t n_args, const mp_obj_t *args) {
         while (self->active != active) {
             MICROPY_EVENT_POLL_HOOK;
         }
+
+        #if MICROPY_PY_NETWORK_WLAN_DPP
+        if (self->if_id == ESP_IF_WIFI_STA && wifi_dpp_state.listening) {
+            esp_exceptions(esp_supp_dpp_stop_listen());
+            wifi_dpp_state.listening = false;
+            wifi_dpp_state.state = DPP_STATE_WAIT;
+            wifi_dpp_state.error = 0;
+        }
+        #endif
     }
 
     return (mode & bit) ? mp_const_true : mp_const_false;
 }
 static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(network_wlan_active_obj, 1, 2, network_wlan_active);
+
+#if MICROPY_PY_NETWORK_WLAN_DPP
+static mp_obj_t network_wlan_dpp_start(size_t n_args, const mp_obj_t *args) {
+    require_if(args[0], ESP_IF_WIFI_STA);
+
+    static int dpp_initialised = 0;
+    if (!dpp_initialised) {
+        #if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 5, 0))
+        esp_exceptions(esp_supp_dpp_init(NULL));
+        #else
+        esp_exceptions(esp_supp_dpp_init(esp_wifi_dpp_callback));
+        #endif
+        dpp_initialised = 1;
+    }
+
+    // check enrollment method, only QR code is supported as of ESP-IDF 5.5.
+    mp_int_t enrollment_method = mp_obj_get_int(args[2]);
+    if (enrollment_method != DPP_BOOTSTRAP_QR_CODE) {
+        mp_raise_ValueError(MP_ERROR_TEXT("invalid enrollment method"));
+    }
+
+    // build channels list
+    size_t channels_count = 0;
+    char *channels = NULL;
+    if (mp_obj_is_tuple_compatible(args[1])) {
+        mp_obj_t *channel_objects;
+        mp_obj_tuple_get(args[1], &channels_count, &channel_objects);
+        if (channels_count >= ESP_DPP_MAX_CHAN_COUNT) {
+            mp_raise_ValueError(MP_ERROR_TEXT("too many channels"));
+        }
+        // This has to be encoded in ASCII...  To be forward-compatible with
+        // 5GHz channel numbers, assume each channel can be up to 3 characters
+        // long - then add separators and NULL terminator.
+        channels = m_new0(char, channels_count * 4);
+        char *current_pointer = channels;
+        char channel_buffer[5] = {};
+        for (size_t channel = 0; channel < channels_count; ++channel) {
+            mp_int_t channel_id = mp_obj_get_int(channel_objects[channel]);
+            if (!MP_FIT_UNSIGNED(8, channel_id)) {
+                mp_raise_ValueError(MP_ERROR_TEXT("invalid channel"));
+            }
+            size_t len = sprintf(channel_buffer, "%d,", channel_id);
+            memcpy(current_pointer, channel_buffer, len);
+            current_pointer += len;
+        }
+        *(current_pointer - 1) = '\0';
+    } else {
+        mp_int_t channel = mp_obj_get_int(args[1]);
+        if (!MP_FIT_UNSIGNED(8, channel)) {
+            mp_raise_ValueError(MP_ERROR_TEXT("invalid channel"));
+        }
+        channels = m_new0(char, 4);
+        sprintf(channels, "%d", channel);
+    }
+
+    // get key if present
+    char *key = NULL;
+    mp_buffer_info_t bufinfo;
+    if (n_args > 3 && args[3] != mp_const_none) {
+        mp_get_buffer_raise(args[3], &bufinfo, MP_BUFFER_READ);
+        if (bufinfo.len != 32) {
+            mp_raise_ValueError(MP_ERROR_TEXT("invalid buffer length"));
+        }
+        key = m_new0(char, 33);
+        memcpy(key, bufinfo.buf, 32);
+    }
+
+    // get info if present
+    char *info = NULL;
+    if (n_args > 4 && args[4] != mp_const_none) {
+        mp_get_buffer_raise(args[4], &bufinfo, MP_BUFFER_READ);
+        info = m_new0(char, bufinfo.len + 1);
+        memcpy(info, bufinfo.buf, bufinfo.len);
+    }
+
+    // Reset the URI buffer first.
+    if (wifi_dpp_state.qr_code_buffer) {
+        heap_caps_free(wifi_dpp_state.qr_code_buffer);
+        wifi_dpp_state.qr_code_buffer = NULL;
+        wifi_dpp_state.qr_code_length = 0;
+    }
+    wifi_dpp_state.state = DPP_STATE_WAIT;
+    wifi_dpp_state.error = 0;
+
+    esp_supp_dpp_stop_listen();  // Just in case.
+    esp_exceptions(esp_supp_dpp_bootstrap_gen(channels, DPP_BOOTSTRAP_QR_CODE, key, info));
+
+    // Loop until the URI is ready.  Since this is algorithmic and not depending
+    // on external factors and expected to finish in reasonable time, looping on
+    // the QR code length is somewhat acceptable.
+
+    while (wifi_dpp_state.qr_code_length == 0) {
+        MICROPY_EVENT_POLL_HOOK;
+    }
+
+    if (wifi_dpp_state.qr_code_length < 0) {
+        // Out of memory whilst duplicating the QR code data.
+        assert(wifi_dpp_state.qr_code_buffer == NULL && "Error reported with non-null buffer");
+        mp_raise_msg(&mp_type_MemoryError, MP_ERROR_TEXT("cannot allocate memory for QR code"));
+    }
+
+    mp_obj_t qr_code_string = mp_obj_new_str(wifi_dpp_state.qr_code_buffer, (size_t)wifi_dpp_state.qr_code_length);
+    heap_caps_free(wifi_dpp_state.qr_code_buffer);
+    wifi_dpp_state.qr_code_buffer = NULL;
+    wifi_dpp_state.qr_code_length = 0;
+
+    wifi_dpp_state.listening = true;
+    esp_err_t error = esp_supp_dpp_start_listen();
+    if (error != ESP_OK) {
+        wifi_dpp_state.listening = false;
+    }
+    esp_exceptions(error);
+
+    return qr_code_string;
+}
+static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(network_wlan_dpp_start_obj, 3, 5, network_wlan_dpp_start);
+
+static mp_obj_t network_wlan_dpp_state(mp_obj_t self_in) {
+    require_if(self_in, ESP_IF_WIFI_STA); // Can be removed if needed.
+
+    // Rather than having a mutex just loop through, it shouldn't make more than
+    // two iterations per value anyway.
+
+    int state;
+    int error;
+
+    for (;;) {
+        int state_check;
+        do {
+            state = wifi_dpp_state.state;
+            state_check = wifi_dpp_state.state;
+        } while (state != state_check);
+
+        int error_check;
+        do {
+            error = wifi_dpp_state.error;
+            error_check = wifi_dpp_state.error;
+        } while (error != error_check);
+
+        // Read between state and error update.
+        if (state == DPP_STATE_ERROR && error == 0) {
+            continue;
+        }
+        break;
+    }
+
+    mp_obj_t payload[2] = {
+        MP_OBJ_NEW_SMALL_INT(state),
+        MP_OBJ_NEW_SMALL_INT(error),
+    };
+
+    return MP_OBJ_FROM_PTR(mp_obj_new_tuple(2, payload));
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(network_wlan_dpp_state_obj, network_wlan_dpp_state);
+
+static mp_obj_t network_wlan_dpp_stop(mp_obj_t self_in) {
+    require_if(self_in, ESP_IF_WIFI_STA);
+
+    if (wifi_dpp_state.listening) {
+        esp_exceptions(esp_supp_dpp_stop_listen());
+        wifi_dpp_state.listening = false;
+        wifi_dpp_state.state = DPP_STATE_WAIT;
+        wifi_dpp_state.error = 0;
+    }
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(network_wlan_dpp_stop_obj, network_wlan_dpp_stop);
+#endif
 
 static mp_obj_t network_wlan_connect(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
     enum { ARG_ssid, ARG_key, ARG_bssid };
@@ -349,6 +621,16 @@ static mp_obj_t network_wlan_connect(size_t n_args, const mp_obj_t *pos_args, mp
             wifi_sta_config.sta.bssid_set = 1;
             memcpy(wifi_sta_config.sta.bssid, p, sizeof(wifi_sta_config.sta.bssid));
         }
+
+        #if MICROPY_PY_NETWORK_WLAN_DPP
+        if (wifi_dpp_state.listening) {
+            esp_exceptions(esp_supp_dpp_stop_listen());
+            wifi_dpp_state.listening = false;
+            wifi_dpp_state.state = DPP_STATE_WAIT;
+            wifi_dpp_state.error = 0;
+        }
+        #endif
+
         esp_exceptions(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_sta_config));
     }
 
@@ -786,6 +1068,11 @@ static const mp_rom_map_elem_t wlan_if_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_config), MP_ROM_PTR(&network_wlan_config_obj) },
     { MP_ROM_QSTR(MP_QSTR_ifconfig), MP_ROM_PTR(&esp_network_ifconfig_obj) },
     { MP_ROM_QSTR(MP_QSTR_ipconfig), MP_ROM_PTR(&esp_nic_ipconfig_obj) },
+    #if MICROPY_PY_NETWORK_WLAN_DPP
+    { MP_ROM_QSTR(MP_QSTR_dpp_start), MP_ROM_PTR(&network_wlan_dpp_start_obj) },
+    { MP_ROM_QSTR(MP_QSTR_dpp_state), MP_ROM_PTR(&network_wlan_dpp_state_obj) },
+    { MP_ROM_QSTR(MP_QSTR_dpp_stop), MP_ROM_PTR(&network_wlan_dpp_stop_obj) },
+    #endif
 
     #if MICROPY_PY_NETWORK_WLAN_CSI
     { MP_ROM_QSTR(MP_QSTR_csi_enable), MP_ROM_PTR(&network_wlan_csi_enable_obj) },
@@ -835,6 +1122,13 @@ static const mp_rom_map_elem_t wlan_if_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_BANDWIDTH_80), MP_ROM_INT(WIFI_BW80) },
     { MP_ROM_QSTR(MP_QSTR_BANDWIDTH_160), MP_ROM_INT(WIFI_BW160) },
     { MP_ROM_QSTR(MP_QSTR_BANDWIDTH_80_80), MP_ROM_INT(WIFI_BW80_BW80) },
+
+    #if MICROPY_PY_NETWORK_WLAN_DPP
+    { MP_ROM_QSTR(MP_QSTR_DPP_METHOD_QR), MP_ROM_INT(DPP_BOOTSTRAP_QR_CODE) },
+    { MP_ROM_QSTR(MP_QSTR_DPP_STATE_WAIT), MP_ROM_INT(DPP_STATE_WAIT) },
+    { MP_ROM_QSTR(MP_QSTR_DPP_STATE_READY), MP_ROM_INT(DPP_STATE_READY) },
+    { MP_ROM_QSTR(MP_QSTR_DPP_STATE_ERROR), MP_ROM_INT(DPP_STATE_ERROR) },
+    #endif
 };
 static MP_DEFINE_CONST_DICT(wlan_if_locals_dict, wlan_if_locals_dict_table);
 
